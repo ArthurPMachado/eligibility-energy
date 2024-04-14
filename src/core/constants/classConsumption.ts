b8b1cc8c46445f8acad3ba2fa43cc51312fa7f97
Arthur Machado <arthur.machado@iclubs.tech>
2024-04-14 03:53:01 -0300

    refactor: use anonymous function to return eligible values

--- src/core/constants/classConsumption.ts
+++ src/core/constants/classConsumption.ts
@@ -1,7 +1,18 @@
-export const classConsumption = {
+export const classesConsumption = {
   residencial: 'residencial',
   industrial: 'industrial',
   comercial: 'comercial',
-  RURAL: 'rural',
+  rural: 'rural',
   poderPublico: 'poderPublico',
 }
+
+/**
+ * The code create a anonymous function and calls right after it, passing the
+ * classesConsumption object as an argument, destructure this object and return the
+ * eligibleConsumptions
+ */
+export const eligibleConsumptions = (({
+  rural,
+  poderPublico,
+  ...restConsumptions
+}) => restConsumptions)(classesConsumption)
--- src/core/constants/taxModality.ts
+++ src/core/constants/taxModality.ts
@@ -1,6 +1,14 @@
-export const taxModality = {
+export const taxModalities = {
   azul: 'azul',
   branca: 'branca',
   verde: 'verde',
   convencional: 'convencional',
 }
+
+/**
+ * The code create a anonymous function and calls right after it, passing the
+ * taxModalities object as an argument, destructure this object and return the
+ * eligibleTaxModalities
+ */
+export const eligibleTaxModalities = (({ azul, verde, ...restModalities }) =>
+  restModalities)(taxModalities)
