954a2f629a3e3e8dafe235a7d60db54cba93b73d
Arthur Machado <arthur.machado@iclubs.tech>
2024-04-15 16:33:25 -0300

    style: rename aws resources names

--- index.ts
+++ index.ts
@@ -9,19 +9,22 @@ const cpu = config.getNumber('cpu') || 512
 const memory = config.getNumber('memory') || 128
 
 // An ECS cluster to deploy into
-const cluster = new aws.ecs.Cluster('cluster', {})
+const cluster = new aws.ecs.Cluster('lemon-cluster', {})
 
 // An ALB to serve the container endpoint to the internet
-const loadbalancer = new awsx.lb.ApplicationLoadBalancer('loadbalancer', {})
+const loadbalancer = new awsx.lb.ApplicationLoadBalancer(
+  'lemon-loadbalancer',
+  {},
+)
 
 // An ECR repository to store our application's container image
-const repo = new awsx.ecr.Repository('repo', {
+const ecrRepository = new awsx.ecr.Repository('docker-repo', {
   forceDelete: true,
 })
 
 // Build and publish our application's container image from ./app to the ECR repository
-const image = new awsx.ecr.Image('image', {
-  repositoryUrl: repo.url,
+const containerImage = new awsx.ecr.Image('docker-image', {
+  repositoryUrl: ecrRepository.url,
   context: './app',
   platform: 'linux/amd64',
 })
@@ -32,8 +35,8 @@ const service = new awsx.ecs.FargateService('service', {
   assignPublicIp: true,
   taskDefinitionArgs: {
     container: {
-      name: 'eligibility-energy',
-      image: image.imageUri,
+      name: 'lemon-energy',
+      image: containerImage.imageUri,
       cpu,
       memory,
       essential: true,
